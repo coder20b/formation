5c9cf6ec9fe63aa3445d6035824908410eaf8ef3
LH <lh@applh.com>
2020-12-03 12:36:15 +0100

    ajout exo3 - etape 2

---
+++ revision-exam/exo3/cinema.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>EXO3 MOVIES</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Formulaire ajout film</h2>
+            <form action="">
+                
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>tous droits réservés</p>
+    </footer>
+</body>
+</html>
