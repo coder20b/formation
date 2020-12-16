3b415ddb6c7a41c007905b3a592382f8a6a4f65b
LH <lh@applh.com>
2020-12-16 15:23:16 +0100

    ajout login

--- projet-cms/php/view/section-login.php
+++ projet-cms/php/view/section-login.php
@@ -1,4 +1,20 @@
 
 <section>
     <h3>FORMULAIRE DE LOGIN</h3>
+    <form method="POST" action="#form-login" id="form-login" enctype="multipart/form-data">
+        <label>
+            <span>email</span>
+            <input type="email" name="email" required placeholder="email" maxlength="160">
+        </label>
+        <label>
+            <span>motDePasse</span>
+            <input type="password" name="motDePasse" required placeholder="motDePasse" maxlength="160">
+        </label>
+        <button type="submit">SE CONNECTER</button>
+        <!-- PARTIE TECHNIQUE -->
+        <input type="hidden" name="formIdentifiant" value="user-login">
+        <div>
+            <?php require_once "php/controller/traitement-user.php" ?>
+        </div>
+    </form>
 </section>
