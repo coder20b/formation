9d41f69018f82513aa3f4f849823adb92303f67a
LH <lh@applh.com>
2021-01-05 11:30:37 +0100

    ajout code symfony

--- symfony-bases/src/Controller/VitrineController.php
+++ symfony-bases/src/Controller/VitrineController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+// NE PAS OUBLIER DE RAJOUTER LES use POUR LES CLASSES
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Contact;
+use App\Form\ContactType;
+
 // POUR UTILISER TWIG ON CREE UN HERITAGE DE CLASSE 
 // AVEC LA CLASSE PARENTE AbstractController
 class VitrineController extends AbstractController
@@ -25,15 +31,30 @@ class VitrineController extends AbstractController
     }
 
     /**
-     * @Route("/contactez-nous", name="contact")
+     * @Route("/contact", name="contact", methods={"GET","POST"})
      */    
-    function contact ()
+    function contact (Request $request): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            // return $this->redirectToRoute('contact_index');
+        }
+
         // LA METHODE render VIENT DE LA CLASSE PARENTE AbstractController
         // ON VA CHARGER LE CODE DU TEMPLATE 
         // templates/vitrine/contact/html.twig
         // (DANS VSCODE AJOUTER UNE EXTENSION POUR LES FICHIERS .twig)
-        return $this->render("vitrine/contact.html.twig");
+        return $this->render("vitrine/contact.html.twig", [
+            'contact'   => $contact,
+            'form'      => $form->createView(),
+        ]);
     }
 
 }
